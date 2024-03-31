bbed4881e2f07dc6d8625e7132ce99ef325c0164
SergiCalaff <88968802+SergiCalaff@users.noreply.github.com>
2024-03-31 12:50:58 +0200

    Update immobles.js
    Implementat el nou us de la funció per verificar si l'usuari es del tipus adeqüat per accedir als diferents endpoints

--- immobles.js
+++ immobles.js
@@ -5,7 +5,7 @@
 
 const express = require("express");
 const db = require('./database');
-const verificaToken = require('./auth');
+const { verificaToken, verificarTipusUsuari } = require('./auth');
 
 const immobles = express();
 immobles.use(express.json());
@@ -77,7 +77,7 @@ immobles.get('/immobles/poblacio/:poblacio', async (req, res) => {
 });
 
 // Endpoint per obtenir la llista d'immobles segons l'ID de l'usuari del token  (R)
-immobles.get('/immobles/r', verificaToken, async (req, res) => {
+immobles.get('/immobles/r', verificaToken, verificarTipusUsuari('R'), async (req, res) => {
     try {
         const idUsuariToken = req.usuario.id_usuari;
 
@@ -87,7 +87,7 @@ immobles.get('/immobles/r', verificaToken, async (req, res) => {
                 return res.status(500).json({ error: 'Error en el servidor' });
             }
             if (!results || results.length === 0) {
-                return res.status(404).json({ error: 'No s\'ha trobat cap immoble per a aquest usuari' });
+                return res.status(404).json({ error: 'No se encontraron resultados' });
             }
             res.json(results);
         });
@@ -99,22 +99,11 @@ immobles.get('/immobles/r', verificaToken, async (req, res) => {
 
 
 // Endpoint per afegir un nou immoble (R)
-immobles.post('/immobles/r/afegir', verificaToken, async (req, res) => {
+immobles.post('/immobles/r/afegir', verificaToken, verificarTipusUsuari('R'), async (req, res) => {
     try {
         // Recollim dades de l'immoble del body, excepte id_usuari que l'agafa del token
         let { Carrer, Numero, Pis, Codi_Postal, Poblacio, Descripcio, Preu, Imatge } = req.body;
         const id_usuari = req.usuario.id_usuari;
-
-        // Netejem les dades rebudes abans de fer la consulta
-        Carrer = netejaDada(Carrer);
-        Numero = netejaDada(Numero);
-        Pis = netejaDada(Pis);
-        Codi_Postal = netejaDada(Codi_Postal);
-        Poblacio = netejaDada(Poblacio);
-        Descripcio = netejaDada(Descripcio);
-        Preu = netejaDada(Preu);
-        Imatge = netejaDada(Imatge);
-
 
         // Consulta SQL per afegir un nou immoble a la bbdd amb l'id de l'usuari autenticat
         db.query('INSERT INTO immobles (id_usuari, Carrer, Numero, Pis, Codi_Postal, Poblacio, Descripcio, Preu, Imatge) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [id_usuari, Carrer, Numero, Pis, Codi_Postal, Poblacio, Descripcio, Preu, Imatge], (err, result) => {
@@ -122,7 +111,7 @@ immobles.post('/immobles/r/afegir', verificaToken, async (req, res) => {
                 console.error("Error:", err);
                 return res.status(500).json("Error del servidor");
             } else {
-                res.status(201).json({ message: 'Nou immoble afegit amb èxit!' });
+                res.status(201).json({ message: 'Nuevo inmueble añadido con éxito' });
             }
         });
     } catch (err) {
@@ -135,20 +124,20 @@ immobles.post('/immobles/r/afegir', verificaToken, async (req, res) => {
 
 
 // Endpoint per eliminar un immoble per la seva id_immoble (R)
-immobles.delete('/immobles/r/eliminar/:id_immoble', verificaToken, async (req, res) => {
+immobles.delete('/immobles/r/eliminar/:id_immoble', verificaToken, verificarTipusUsuari('R'), async (req, res) => {
     
     // Recollim el id_usuari del token i la id_immoble de la URL
     const { id_usuari, TipusUsuari } = req.usuario;
-    const id_immoble_param = parseInt(req.params.id_immoble); // Diferenciem el paràmetre id_immoble
+    const id_immoble_param = parseInt(req.params.id_immoble);
 
     // Verifiquem si el tipus d'usuari és "R"
     if (TipusUsuari !== "R") {
-        return res.status(403).json({ error: 'Usuari no registrat' });
+        return res.status(403).json({ error: 'Usuario no registrado' });
     }
 
     // Verifiquem si la id_immoble és un número vàlid
     if (isNaN(id_immoble_param) || id_immoble_param <= 0) {
-        return res.status(400).json({ error: 'La ID de l\'immoble no és vàlida' });
+        return res.status(400).json({ error: 'La ID del inmueble no és vàlida' });
     }
 
     try {
@@ -159,7 +148,7 @@ immobles.delete('/immobles/r/eliminar/:id_immoble', verificaToken, async (req, r
                 console.error("Error en la consulta SELECT:", err);
                 reject({ status: 500, message: 'Error del servidor' });
             } else if (!result || result.length === 0) {
-                reject({ status: 404, message: 'No s\'ha trobat cap immoble amb aquesta ID per a aquest usuari' });
+                reject({ status: 404, message: 'No se encontraron resultados' });
             } else {
                 resolve(result);
             }
@@ -178,55 +167,14 @@ immobles.delete('/immobles/r/eliminar/:id_immoble', verificaToken, async (req, r
         });
     });
 
-    res.status(200).json({ message: 'Immoble eliminat amb èxit' });
+    res.status(200).json({ message: 'Inmueble eliminado con éxito' });
 } catch (error) {
     res.status(error.status || 500).json({ error: error.message });
 }
 });
 
-// Endpoint per a la llista de tots els usuaris (A)
-immobles.get('/immobles/a/llistaUsuaris', verificaToken, async (req, res) => {
-    try {
-        db.query('SELECT * FROM usuaris', (error, results) => {
-            if (error) {
-                console.error("Error:", error);
-                return res.status(500).json({ error: 'Error en el servidor' });
-            }
-            if (!results || results.length === 0) {
-                return res.status(404).json({ error: 'No s\'ha trobat cap usuari' });
-            }
-            res.json(results);
-        });
-    } catch (error) {
-        console.error("Error en el bloc catch:", error);
-        res.status(500).json("Error del servidor");
-    }
-});
-
-
-// Endpoint per a la llista d'usuaris per Email usuari (A)
-immobles.get('/immobles/a/llistaUsuaris/:Email', verificaToken, async (req, res) => {
-    try {
-        const Email = req.params.Email;
-
-        db.query('SELECT * FROM usuaris WHERE Email = ?', [Email], (error, results) => {
-            if (error) {
-                console.error("Error:", error);
-                return res.status(500).json({ error: 'Error en el servidor' });
-            }
-            if (!results || results.length === 0) {
-                return res.status(404).json({ error: 'No s\'ha trobat cap usuari' });
-            }
-            res.json(results);
-        });
-    } catch (error) {
-        console.error("Error en el bloc catch:", error);
-        res.status(500).json("Error del servidor");
-    }
-});
-
 // Endpoint per a la llista d'immobles per Email usuari (A)
-immobles.get('/immobles/a/llistaImmobles/:Email', verificaToken, (req, res) => {
+immobles.get('/immobles/a/llistaImmobles/:Email', verificaToken, verificarTipusUsuari('A'), async(req, res) => {
     const Email = req.params.Email;
 
     // Consulta per obtenir els immobles associats a l'usuari amb l'email proporcionat
@@ -243,7 +191,7 @@ immobles.get('/immobles/a/llistaImmobles/:Email', verificaToken, (req, res) => {
             return res.status(500).json({ error: 'Error en el servidor' });
         }
         if (!results || results.length === 0) {
-            return res.status(404).json({ error: 'No s\'han trobat immobles per a aquest usuari' });
+            return res.status(404).json({ error: 'No se encontraron resultados' });
         }
 
         res.json(results);
@@ -255,7 +203,7 @@ immobles.get('/immobles/a/llistaImmobles/:Email', verificaToken, (req, res) => {
 
 
 // Endpoint per afegir un immoble a un usuari (A)
-immobles.post('/immobles/a/afegirUsuariImmoble/:id_usuari', verificaToken, async (req, res) => {
+immobles.post('/immobles/a/afegirUsuariImmoble/:id_usuari', verificaToken, verificarTipusUsuari('A'), async (req, res) => {
     // Verificar que l'usuari que fa la sol·licitud sigui de tipus "A"
     if (req.usuario.TipusUsuari !== 'A') {
         return res.status(403).json({ error: 'Acceso prohibido' });
@@ -268,23 +216,13 @@ immobles.post('/immobles/a/afegirUsuariImmoble/:id_usuari', verificaToken, async
         // Recollim dades de l'immoble del body
         let { Carrer, Numero, Pis, Codi_Postal, Poblacio, Descripcio, Preu, Imatge } = req.body;
 
-        // Netejem les dades rebudes abans de fer la consulta
-        Carrer = netejaDada(Carrer);
-        Numero = netejaDada(Numero);
-        Pis = netejaDada(Pis);
-        Codi_Postal = netejaDada(Codi_Postal);
-        Poblacio = netejaDada(Poblacio);
-        Descripcio = netejaDada(Descripcio);
-        Preu = netejaDada(Preu);
-        Imatge = netejaDada(Imatge);
-
         // Consulta SQL per afegir un nou immoble a la bbdd amb l'id de l'usuari rebut per URL
         db.query('INSERT INTO immobles (id_usuari, Carrer, Numero, Pis, Codi_Postal, Poblacio, Descripcio, Preu, Imatge) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [id_usuari, Carrer, Numero, Pis, Codi_Postal, Poblacio, Descripcio, Preu, Imatge], (err, result) => {
             if (err) {
                 console.error("Error en la inserció d'immoble:", err);
                 return res.status(500).json({ error: 'Error del servidor' });
             } else {
-                res.status(201).json({ message: 'Nou immoble afegit amb èxit!' });
+                res.status(201).json({ message: 'Nuevo inmueble añadido con éxito' });
             }
         });
     } catch (err) {
@@ -293,38 +231,10 @@ immobles.post('/immobles/a/afegirUsuariImmoble/:id_usuari', verificaToken, async
     }
 });
 
-/*
-// Endpoint per eliminar un usuari (A)
-immobles.delete('/immobles/a/eliminarUsuari/:id_usuari', verificaToken, (req, res) => {
-    
-    if (req.usuario.TipusUsuari !== 'A') {
-        return res.status(403).json({ error: 'Acceso prohibido' }); 
-    }
 
-    const id_usuari = parseInt(req.params.id_usuari);
-
-    if (isNaN(id_usuari) || id_usuari <= 0) {
-        return res.status(400).json({ error: 'El ID del usuario no es válido' }); 
-    }
-
-    // Executar la consulta SQL per eliminar l'usuari de la base de dades
-    db.query('DELETE FROM usuaris WHERE id_usuari = ?', [id_usuari], (err, result) => {
-        if (err) {
-            console.error("Error:", err);
-            return res.status(500).json({ error: 'Error del servidor' }); 
-        } else {
-            if (result.affectedRows === 0) {
-                return res.status(404).json({ error: 'No se ha encontrado ningún usuario con este ID' }); 
-            } else {
-                res.status(200).json({ message: 'Usuario eliminado con éxito' }); 
-            }
-        }
-    });
-});
-*/
 
 // Endpoint per eliminar un immoble (A)
-immobles.delete('/immobles/a/eliminarImmoble/:id_immoble/:id_usuari', verificaToken, async (req, res) => {
+immobles.delete('/immobles/a/eliminarImmoble/:id_immoble/:id_usuari', verificaToken, verificarTipusUsuari('A'), async (req, res) => {
     // Verificar que l'usuari que fa la sol·licitud sigui de tipus "A"
     if (req.usuario.TipusUsuari !== 'A') {
         return res.status(403).json({ error: 'Acceso prohibido' });
@@ -342,7 +252,7 @@ immobles.delete('/immobles/a/eliminarImmoble/:id_immoble/:id_usuari', verificaTo
                     console.error("Error en la consulta SELECT:", err);
                     reject({ status: 500, message: 'Error del servidor' });
                 } else if (!result || result.length === 0) {
-                    reject({ status: 404, message: 'No s\'ha trobat cap immoble amb aquesta ID per a aquest usuari' });
+                    reject({ status: 404, message: 'No se encontraron resultados' });
                 } else {
                     resolve(result[0]); // Només necessitem un resultat
                 }
@@ -361,26 +271,12 @@ immobles.delete('/immobles/a/eliminarImmoble/:id_immoble/:id_usuari', verificaTo
             });
         });
 
-        res.status(200).json({ message: 'Immoble eliminat amb èxit' });
+        res.status(200).json({ message: 'Inmueble eliminado con éxito' });
     } catch (error) {
         res.status(error.status || 500).json({ error: error.message });
     }
-});
 
-// Funció per netejar les cadenes de text en cas de trobar caràcters especials i espais en blanc al principi
-function netejaDada(dada) {
-    if (typeof dada === 'string') {
-        dada = dada.trim();
-        dada = dada.replace(/\\+/g, '\\');
-        dada = dada.replace(/&/g, '&amp;')
-                   .replace(/</g, '&lt;')
-                   .replace(/>/g, '&gt;')
-                   .replace(/"/g, '&quot;')
-                   .replace(/'/g, '&#39;');
-    }
-    return dada;
-}
+});
 
 
 module.exports = immobles;
-
