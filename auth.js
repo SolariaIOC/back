ddd3266fb6a095ae91ebaad8704466008b188469
SergiCalaff <88968802+SergiCalaff@users.noreply.github.com>
2024-03-31 12:47:51 +0200

    Update auth.js
    Simplificat i convertit a funció el comprovar si els usuaris estan autoritzats.

--- auth.js
+++ auth.js
@@ -1,11 +1,5 @@
-//Treballem amb 3 tipus d'usuari
-// anónim = (accesible per tothom)
-// Registrat = (R) nomes pot accedir a les seves dades
-// Administrador = (A) pot accedir a tots els usuaris i immobles
-
 const jwt = require("jsonwebtoken");
-const { refrescarToken } = require('./tokenUtils.js')
-
+const { refrescarToken } = require('./tokenUtils.js');
 
 // Middleware per verificar el token i obtenir l'ID de l'usuari
 function verificaToken(req, res, next) {
@@ -17,76 +11,10 @@ function verificaToken(req, res, next) {
             return res.status(401).json({ error: 'Token no proporcionat' });
         }
 
-        // Verifica que s'ha proporcionat el token correctament
+        // Verifica que s'ha proporcionat el token correctament i guarda les dades de l'usuari
         const decodedToken = jwt.verify(authorizationHeader, process.env.JWT_SECRET);
-        req.usuario = {
-            id_usuari: decodedToken.usuario.id_usuari,
-            TipusUsuari: decodedToken.usuario.TipusUsuari
-        };
-
-        // Redirigeix l'usuari al seu endpoint corresponent 
-        //si no s'està intentant accedir ja a l'endpoint adequat
-        if (req.method !== 'DELETE') {
-            switch (req.usuario.TipusUsuari) {
-                case 'R':
-                    switch (req.method) {
-                        case 'GET':
-                            if (req.path !== '/immobles/r') {
-                                return res.redirect('/immobles/r');
-                            }
-                            break;
-                        case 'POST':
-                            if (req.path !== '/immobles/r/afegir') {
-                                return res.redirect('/immobles/r/afegir');
-                            }
-                            break;
-                        case 'DELETE':
-                            if (req.path !== '/immobles/r/eliminar') {
-                                return res.redirect('/immobles/r/eliminar');
-                            }
-                            break;
-                    }
-                    break;
-                case 'A':
-                    switch (req.method) {
-                        case 'GET':
-                            if (req.path === '/immobles/a/llistaUsuaris') {
-                                return res.redirect('/immobles/a/llistaUsuaris');
-                            } else if (req.path === '/immobles/a/llistaUsuaris/:Email') {
-                                return res.redirect('/immobles/a/llistaUsuaris/:Email');
-                            } else if (req.path === '/immobles/a/llistaImmobles') {
-                                return res.redirect('/immobles/a/llistaImmobles');
-                            } else if (req.path === '/immobles/a/llistaImmobles/:id_immoble') {
-                                return res.redirect('/immobles/a/llistaImmobles/:id_immoble');
-                            } else if (req.path === '/immobles/a/llistaImmobles/:Email') {
-                                return res.redirect('/immobles/a/llistaImmobles/:Email');
-                            }
-                            break;
-                        case 'POST':
-                            console.log("METODE dins CASE POST ", req.method);
-                            if (req.path === '/usuaris/app/registre') {
-                                return res.redirect('/usuaris/app/registre');
-                            } else if (req.path === '/immobles/a/afegirUsuariImmoble') {
-                                return res.redirect('/immobles/a/afegirImmoble');
-                            }
-                            break;
-                        case 'DELETE':
-                            if (req.path === '/usuaris/app/a/eliminarUsuari/:id_usuari') {
-                                return res.redirect('/usuaris/app/a/eliminarUsuari/:id_usuari');
-                            } else if (req.path === '/immobles/a/eliminarImmoble/:id_immoble/:id_usuari') {
-                                return res.redirect('/immobles/a/eliminarImmoble/:id_immoble/:id_usuari');
-                            }
-                            break;
-                    }
-                    break;
-                default:
-                    return res.status(403).json({ error: 'Acceso prohibido' });
-                    break;
-            }
-            next();
-        } else {
-            next(); // En cas de petició DELETE continua
-        }
+        req.usuario = decodedToken.usuario;
+        next();
 
     } catch (error) {
         if (!refreshToken) {
@@ -96,7 +24,7 @@ function verificaToken(req, res, next) {
         try {
             const { accessToken, usuario, error: refreshError } = refrescarToken(refreshToken);
             if (refreshError) {
-                res.status(302).json({ redirectTo: "http://solaria.website" }) // Redirigir a la pàgina de login
+                res.status(302).json({ redirectTo: "http://solaria.website" }); // Redirigir a la pàgina de login
             }
             req.usuario = usuario;
             res
@@ -108,6 +36,25 @@ function verificaToken(req, res, next) {
         }
     }
 }
+
+// Funció per verificar el tipus d'usuari
+function verificarTipusUsuari(tipusUsuariPermes) {
+    return (req, res, next) => {
+        try {
+            // Verificar si el tipus d'usuari del token coincideix amb el permès o si es null o undefined
+            if (!req.usuario || (tipusUsuariPermes && req.usuario.TipusUsuari !== tipusUsuariPermes)) {
+                return res.status(403).json({ error: 'Acceso prohibido' });
+            }
+            // Si coincideix, permetre que continuï l'execució
+            next();
+        } catch (error) {
+            return res.status(500).json({ error: 'Error en la verificación del tipo de usuario' });
+        }
+    };
+}
+
+module.exports = { verificaToken, verificarTipusUsuari };
+
 
 module.exports = verificaToken;
 
